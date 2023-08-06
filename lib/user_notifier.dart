369e6475dc9437ff1d7e55067873ce87cd719c2a
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 20:46:17 +0900

    impl UserNotifier

--- lib/user_notifier.dart
+++ lib/user_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 part 'user_notifier.freezed.dart';
 
@@ -7,4 +8,10 @@ class UserState with _$UserState {
   const factory UserState({
     @Default('名前未設定') String name,
   }) = _UserState;
+}
+
+class UserNotifier extends StateNotifier<UserState> {
+  UserNotifier(this._ref) : super(const UserState());
+
+  final Ref _ref;
 }
