fa5fba324cc853fa710b737e0599572c2115f304
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 23:39:53 +0900

    fix

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -50,7 +50,7 @@ class _Tile extends StatelessWidget {
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(leadingSize / 2),
         child: Image.network(
-          item.imagePath,
+          item.userItem.imagePath ?? '',
           width: leadingSize,
           height: leadingSize,
           errorBuilder: (_, __, ___) {
@@ -63,7 +63,7 @@ class _Tile extends StatelessWidget {
         ),
       ),
       title: Text(
-        item.name,
+        item.userItem.name,
         maxLines: 1,
         overflow: TextOverflow.ellipsis,
       ),
