8bce02a7d080bc54f3854b4db748010132fd49f2
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 17:46:13 +0900

    impl

--- lib/write_screen.dart
+++ lib/write_screen.dart
@@ -6,48 +6,53 @@ class WriteScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Details Screen'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextFormField(
-                maxLines: null,
-                autofocus: true,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Details Screen'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextFormField(
+                  maxLines: null,
+                  autofocus: true,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Material(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(4),
-                child: InkWell(
-                  onTap: () {},
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Material(
+                  color: Colors.blue,
                   borderRadius: BorderRadius.circular(4),
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      child: Text(
-                        '投稿',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
+                  child: InkWell(
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(4),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                        child: Text(
+                          '投稿',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
