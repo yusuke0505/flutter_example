373f173737b3c21184bb3e4745d696519d21a8c6
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 22:36:43 +0900

    impl

--- lib/name_edit/name_edit_screen.dart
+++ lib/name_edit/name_edit_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_example/name_edit/name_edit_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NameEditScreen extends StatelessWidget {
+class NameEditScreen extends HookConsumerWidget {
   const NameEditScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(nameEditNotifierProvider);
+    final notifier = ref.watch(nameEditNotifierProvider.notifier);
     return WillPopScope(
       onWillPop: () async => true,
       child: Scaffold(
