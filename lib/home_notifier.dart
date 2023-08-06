d77d13f36c473c96dceeea54714eae5b72f82adc
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 18:50:32 +0900

    create home notifier

--- lib/home_notifier.dart
+++ lib/home_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod/riverpod.dart';
 
 part 'home_notifier.freezed.dart';
 
@@ -7,4 +8,10 @@ class HomeState with _$HomeState {
   const factory HomeState({
     @Default(true) bool loading,
   }) = _HomeState;
+}
+
+class HomeNotifier extends StateNotifier<HomeState> {
+  HomeNotifier(this._ref) : super(const HomeState());
+
+  final Ref _ref;
 }
