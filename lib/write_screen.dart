2811956e91e8870d4208086ebc7ec2f95b406ae1
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-06 17:48:33 +0900

    refactor

--- lib/write_screen.dart
+++ lib/write_screen.dart
@@ -27,31 +27,47 @@ class WriteScreen extends HookWidget {
               ),
               Padding(
                 padding: const EdgeInsets.all(16),
-                child: Material(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(4),
-                  child: InkWell(
-                    onTap: () {},
-                    borderRadius: BorderRadius.circular(4),
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                        child: Text(
-                          '投稿',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                child: _Button(
+                  onTap: () {},
                 ),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    const radius = 4.0;
+    return Material(
+      color: Colors.blue,
+      borderRadius: BorderRadius.circular(radius),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(radius),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(radius),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Text(
+              '投稿',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
           ),
         ),
       ),
