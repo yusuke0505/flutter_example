2175a2d0ad39bf33fdfe44297901d03f352f2b68
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-07 11:58:23 +0900

    fix

--- lib/my_root_screen.dart
+++ lib/my_root_screen.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_example/tab_item.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class GoRouterStateExtra {
   const GoRouterStateExtra({
@@ -13,7 +11,7 @@ class GoRouterStateExtra {
   final bool showNavBar;
 }
 
-class MyRootScreen extends HookConsumerWidget {
+class MyRootScreen extends StatelessWidget {
   const MyRootScreen({
     this.extra = const GoRouterStateExtra(),
     required this.navigationShell,
@@ -26,42 +24,35 @@ class MyRootScreen extends HookConsumerWidget {
   final GoRouterState goRouterState;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isLoggedIn = useState<bool?>(null);
-    useEffect(() {
-      isLoggedIn.value = FirebaseAuth.instance.currentUser != null;
-      return null;
-    }, const []);
+  Widget build(BuildContext context) {
+    final isLoggedIn = FirebaseAuth.instance.currentUser != null;
     const notShowNabVarPaths = [
       '$homePath/$writePath',
       '$myPagePath/$nameEditPath',
     ];
     final showNavBar = !notShowNabVarPaths.contains(goRouterState.uri.path);
-    return isLoggedIn.value == null
-        ? const Center(child: CircularProgressIndicator())
-        : isLoggedIn.value!
-            ? Scaffold(
-                body: navigationShell,
-                bottomNavigationBar: showNavBar
-                    ? BottomNavigationBar(
-                        items: <BottomNavigationBarItem>[
-                          for (var i = 0; i < TabItem.values.length; i++)
-                            BottomNavigationBarItem(
-                              icon: TabItem.values[i].icon,
-                              label: TabItem.values[i].label,
-                            ),
-                        ],
-                        currentIndex: navigationShell.currentIndex,
-                        onTap: (index) {
-                          navigationShell.goBranch(
-                            index,
-                            initialLocation:
-                                index == navigationShell.currentIndex,
-                          );
-                        },
-                      )
-                    : null,
-              )
-            : const Text('未ログイン');
+    return isLoggedIn
+        ? Scaffold(
+            body: navigationShell,
+            bottomNavigationBar: showNavBar
+                ? BottomNavigationBar(
+                    items: <BottomNavigationBarItem>[
+                      for (var i = 0; i < TabItem.values.length; i++)
+                        BottomNavigationBarItem(
+                          icon: TabItem.values[i].icon,
+                          label: TabItem.values[i].label,
+                        ),
+                    ],
+                    currentIndex: navigationShell.currentIndex,
+                    onTap: (index) {
+                      navigationShell.goBranch(
+                        index,
+                        initialLocation: index == navigationShell.currentIndex,
+                      );
+                    },
+                  )
+                : null,
+          )
+        : const Scaffold(body: Center(child: Text('未ログイン')));
   }
 }
