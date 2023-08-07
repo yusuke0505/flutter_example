d1ad5cf0f8b2c6d022c6a3716c905efb6d9f823d
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-07 12:28:46 +0900

    refactor

--- lib/my_root_screen.dart
+++ lib/my_root_screen.dart
@@ -54,41 +54,50 @@ class MyRootScreen extends StatelessWidget {
                   )
                 : null,
           )
-        : Scaffold(
-            appBar: AppBar(
-              title: const Text('新規登録/ログイン'),
+        : const SignUpScreen();
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('新規登録/ログイン'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextFormField(),
             ),
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: TextFormField(),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: TextFormField(),
-                  ),
-                  const SizedBox(height: 40),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      PostButton(
-                        onTap: () {},
-                        enable: true,
-                        label: '新規登録',
-                      ),
-                      const SizedBox(width: 40),
-                      PostButton(
-                        onTap: () {},
-                        enable: true,
-                        label: 'ログイン',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextFormField(),
             ),
-          );
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PostButton(
+                  onTap: () {},
+                  enable: true,
+                  label: '新規登録',
+                ),
+                const SizedBox(width: 40),
+                PostButton(
+                  onTap: () {},
+                  enable: true,
+                  label: 'ログイン',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
