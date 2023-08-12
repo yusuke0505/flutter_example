db13e2130734e418e7710d6c3eb43efa2e51fdc8
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-12 15:25:04 +0900

    refactor

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example/home/home_notifier.dart';
 import 'package:flutter_example/tab_item.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -12,10 +11,6 @@ class HomeScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(homeNotifierProvider);
     final notifier = ref.watch(homeNotifierProvider.notifier);
-    useEffect(() {
-      notifier.fetch();
-      return null;
-    }, const []);
     final body = state.when(
       data: (data) {
         return RefreshIndicator(
