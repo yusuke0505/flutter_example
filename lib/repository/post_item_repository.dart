537fd9830abc4308324b4e286cbb5fa6b5220240
yusuke0505 <linxxxlinxxxlinxxx@gmail.com>
2023-08-17 11:22:00 +0900

    impl

--- lib/repository/post_item_repository.dart
+++ lib/repository/post_item_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_example/data/post_item/post_item.dart';
-import 'package:flutter_example/data/user/user.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final postItemRepositoryProvider = Provider((_) => PostItemRepository());
@@ -9,11 +8,10 @@ class PostItemRepository {
   final _instance = FirebaseFirestore.instance;
   static const _collectionPath = 'post_items';
 
-  Future<User?> fetch(String uid) async {
+  Future<List<PostItem>?> fetchList() async {
     try {
-      final snapshot =
-          await _instance.collection(_collectionPath).doc(uid).get();
-      return User.fromFirestore(snapshot, null);
+      final snapshot = await _instance.collection(_collectionPath).get();
+      return snapshot.docs.map((e) => PostItem.fromFirestore(e, null)).toList();
     } on Exception {
       return null;
     }
